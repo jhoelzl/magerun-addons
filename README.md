c1c4148d2c0610be62f877639d87eec7e31622b5
josef.hoelzl <josef.hoelzl@anton-paar.com>
2015-06-01 10:54:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+MageRun Addons
+==============
+
+Some additional commands for the excellent N98-MageRun Magento command-line tool.
+
+Installation
+------------
+There are a few options.  You can check out the different options in the [MageRun
+docs](http://magerun.net/introducting-the-new-n98-magerun-module-system/).
+
+Here's the easiest:
+
+1. Create ~/.n98-magerun/modules/ if it doesn't already exist.
+
+        mkdir -p ~/.n98-magerun/modules/
+
+2. Clone the magerun-addons repository in there
+
+        cd ~/.n98-magerun/modules/ && git clone https://github.com/jhoelzl/magerun-addons
+
+3. It should be installed. To see that it was installed, check to see if one of the new commands is in there, like `customer:clean-address-vat-id`.
+
+        n98-magerun.phar customer:clean-address-vat-id
+
+Commands
+--------
+
+    
+### Clean up customers' address VAT-ID field ###
+
+A large number of customers enter their Tax/VAT number incorrectly. Common mistakes are using dots and/or spaces. This command loops through the taxvat fields already in the database and cleans them up. So 'nl 01.23.45.67 b01' (which won't validate) will become '01234567B01' (which will validate). This is useful for future purchases by these customers.
+
+    $ n98-magerun.phar customer:clean-address-vat-id
+    
+    
+Credits due where credits due
+--------
+
+The command is based on https://github.com/peterjaap/magerun-addons and modified to work with VAT field on table customer_address_entity_varchar and not on customer_entity_varchar.
---
+++ src/JH/Magento/Command/Customer/CleanAddressVatIDCommand.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace JH\Magento\Command\Customer;
+
+use N98\Magento\Command\AbstractMagentoCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CleanAddressVatIDCommand extends AbstractMagentoCommand
+{
+    protected function configure()
+    {
+      $this
+          ->setName('customer:clean-address-vat-id')
+          ->setDescription('Clean up Vat-ID address attribute (with code \'vat_id\') by stripping spaces and dots.')
+      ;
+    }
+
+   /**
+    * @param \Symfony\Component\Console\Input\InputInterface $input
+    * @param \Symfony\Component\Console\Output\OutputInterface $output
+    * @return int|void
+    */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->detectMagento($output);
+        if ($this->initMagento()) {
+            $dialog = $this->getHelper('dialog');
+            $resource = \Mage::getSingleton('core/resource');
+            $db = $resource->getConnection('core_write');
+            
+            $countryList = \Mage::getResourceModel('directory/country_collection')
+                    ->loadData()
+                    ->toOptionArray(false);
+            
+            $countryCodes = array();
+            foreach($countryList as $country) {
+                $countryCodes[] = $country['value'];
+            }
+            $this->countryCodes = $countryCodes;
+            
+            $taxVatAttributeId = $db->fetchOne("SELECT attribute_id FROM {$resource->getTableName('eav/attribute')} 
+                WHERE attribute_code = ?", array('vat_id'));
+                
+            $rows = $db->fetchAll("SELECT * FROM {$resource->getTableName('customer_address_entity_varchar')} 
+                WHERE entity_type_id = ? AND attribute_id = ?
+                AND `value` IS NOT NULL", array(2, $taxVatAttributeId));
+            
+            foreach($rows as $row) {
+                // Clean taxvat
+                $newTaxvat = $this->clean($row['value']);
+                // Set new taxvat
+                $db->update($resource->getTableName('customer_address_entity_varchar'), array(
+                    'value' => $newTaxvat,
+                ), 'value_id = ' . $row['value_id']);
+                $output->writeln('<info>Taxvat for customer address ' . $row['entity_id'] .' updated from ' . $row['value'] . ' to ' . $newTaxvat . '</info>');
+            }
+        }
+    }
+
+    private function clean($taxvat) {
+        // For some reason, a lot of people put their email address in the taxvat field (browser autofill?)
+        if (\Zend_Validate::is($taxvat, 'EmailAddress')) {
+            return null;
+        }
+        // Strip dots, commas, spaces and braces
+        $taxvat = preg_replace('/[.,()\s]/', '', $taxvat);
+        // If the string consists of only letters, return empty
+        if (!preg_match('/[^A-Za-z]+/', $taxvat)) {
+            return null;
+        }
+		/*
+        if(in_array(strtoupper(substr($taxvat,0,2)), $this->countryCodes)) {
+            // First two characters matches with a country code, strip it
+			$taxvat = substr($taxvat,2);
+        }*/
+		
+        // Taxvats cannot be shorter than 5 characters
+        if(strlen($taxvat) < 5) {
+            return null;
+        }
+        return strtoupper($taxvat);
+    }
+}
